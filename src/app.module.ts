b73f7a5952a2409aec78b6c59fbecd2ba3319b85
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-06-01 14:33:49 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+    imports: [
+        UsersModule,
+        AuthModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            models: [],
+            autoLoadModels: true,
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
