5effffc2a8e0a75d855cfcbcf25fb28d121e6ce8
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-06-07 07:26:45 +0300

    Add validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder } from '@nestjs/swagger'
 import { SwaggerModule } from '@nestjs/swagger/dist'
+import { ValidationPipe } from './common/pipes/validation.pipe'
 
 async function start() {
     const PORT = process.env.PORT || 5000
@@ -17,6 +18,8 @@ async function start() {
         .build()
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('/api/docs', app, document)
+
+    app.useGlobalPipes(new ValidationPipe())
 
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
